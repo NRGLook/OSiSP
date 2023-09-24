a26e70e30e2571cba3f7d30e5167e7dc413b9354
NRGLook <ilya.tihanyonok@yandex.by>
2023-09-24 13:12:50 +0300

    Add project files.

---
+++ LR1/LR1/LR1/LR1.cpp
@@ -0,0 +1,244 @@
+﻿#include <windows.h>
+#include <vector>
+#include <ctime>
+#include <string>
+
+#define MAX_LOADSTRING 100
+#define IDS_APP_TITLE 101
+
+HINSTANCE hInst;
+HWND hWnd;
+HWND hRestartButton; // Добавлено окно кнопки рестарта
+const int gridSize = 20;
+int width = 20; // Ширина и высота поля
+int height = 15;
+std::vector<POINT> snake;
+POINT food;
+int directionX = 1;
+int directionY = 0;
+bool gameOver = false;
+int foodCount = 0;
+
+ATOM MyRegisterClass(HINSTANCE hInstance);
+BOOL InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void UpdateGame();
+void DrawGame(HDC hdc);
+void CreateFood();
+void RestartGame(); // Добавлена функция перезапуска игры
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow) {
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    hInst = hInstance;
+    MyRegisterClass(hInstance);
+
+    if (!InitInstance(hInstance, nCmdShow)) {
+        return FALSE;
+    }
+
+    MSG msg;
+    UINT_PTR timerId = SetTimer(hWnd, 1, 100, NULL);
+
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    KillTimer(hWnd, timerId);
+
+    return (int)msg.wParam;
+}
+
+ATOM MyRegisterClass(HINSTANCE hInstance) {
+    WNDCLASSEXW wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = nullptr;
+    wcex.lpszClassName = L"SnakeGame";
+    wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
+    return RegisterClassExW(&wcex);
+}
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+    hInst = hInstance;
+    WCHAR szTitle[MAX_LOADSTRING];
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    hWnd = CreateWindow(L"SnakeGame", szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd) {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    width = (GetSystemMetrics(SM_CXSCREEN) - 100) / gridSize; // Ширина поля зависит от размеров экрана
+    height = (GetSystemMetrics(SM_CYSCREEN) - 100) / gridSize; // Высота поля зависит от размеров экрана
+
+    snake.push_back({ width / 2, height / 2 });
+    CreateFood();
+
+    // Создание кнопки рестарта
+    hRestartButton = CreateWindow(L"BUTTON", L"Restart", WS_CHILD | WS_VISIBLE, 10, 10, 100, 30, hWnd, (HMENU)1, hInstance, NULL);
+
+    return TRUE;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        DrawGame(hdc);
+        EndPaint(hWnd, &ps);
+    }
+                 break;
+    case WM_KEYDOWN: {
+        switch (wParam) {
+        case VK_LEFT:
+            if (directionX == 0) {
+                directionX = -1;
+                directionY = 0;
+            }
+            break;
+        case VK_UP:
+            if (directionY == 0) {
+                directionX = 0;
+                directionY = -1;
+            }
+            break;
+        case VK_RIGHT:
+            if (directionX == 0) {
+                directionX = 1;
+                directionY = 0;
+            }
+            break;
+        case VK_DOWN:
+            if (directionY == 0) {
+                directionX = 0;
+                directionY = 1;
+            }
+            break;
+        case 'R': // Обработка клавиши 'R' для рестарта игры
+            RestartGame();
+            break;
+        }
+    }
+                   break;
+    case WM_TIMER:
+        if (gameOver) {
+            KillTimer(hWnd, 1);
+        }
+        else {
+            UpdateGame();
+        }
+        break;
+    case WM_COMMAND: // Обработка сообщений от кнопки рестарта
+        if (LOWORD(wParam) == 1) {
+            RestartGame();
+        }
+        break;
+    case WM_CLOSE: // Обработка закрытия окна
+        KillTimer(hWnd, 1);
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+void UpdateGame() {
+    POINT newHead = snake.front();
+    newHead.x += directionX;
+    newHead.y += directionY;
+    snake.insert(snake.begin(), newHead);
+
+    if (newHead.x == food.x && newHead.y == food.y) {
+        foodCount++;
+        CreateFood();
+        WCHAR szTitle[MAX_LOADSTRING];
+        wsprintf(szTitle, L"SnakeGame - Food: %d", foodCount);
+        SetWindowText(hWnd, szTitle);
+
+        if (foodCount % 5 == 0) {
+            MessageBox(hWnd, L"Поздравляем! Вы собрали 5 яблок!", L"Поздравление", MB_OK);
+        }
+    }
+    else {
+        snake.pop_back();
+    }
+
+    if (newHead.x < 0 || newHead.x >= width || newHead.y < 0 || newHead.y >= height) {
+        gameOver = true;
+    }
+
+    for (size_t i = 1; i < snake.size(); i++) {
+        if (snake[i].x == newHead.x && snake[i].y == newHead.y) {
+            gameOver = true;
+        }
+    }
+
+    if (gameOver) {
+        MessageBox(hWnd, L"Game Over", L"Game Over", MB_OK);
+    }
+
+    InvalidateRect(hWnd, nullptr, TRUE);
+}
+
+void DrawGame(HDC hdc) {
+    HBRUSH greenBrush = CreateSolidBrush(RGB(0, 128, 0));
+    HBRUSH redBrush = CreateSolidBrush(RGB(255, 0, 0));
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    FillRect(hdc, &rect, (HBRUSH)(COLOR_WINDOW + 1));
+
+    for (const auto& segment : snake) {
+        rect.left = segment.x * gridSize;
+        rect.top = segment.y * gridSize;
+        rect.right = rect.left + gridSize;
+        rect.bottom = rect.top + gridSize;
+        FillRect(hdc, &rect, greenBrush);
+    }
+
+    rect.left = food.x * gridSize;
+    rect.top = food.y * gridSize;
+    rect.right = rect.left + gridSize;
+    rect.bottom = rect.top + gridSize;
+    FillRect(hdc, &rect, redBrush);
+
+    DeleteObject(greenBrush);
+    DeleteObject(redBrush);
+}
+
+void CreateFood() {
+    srand(static_cast<unsigned int>(time(nullptr)));
+    food.x = rand() % width;
+    food.y = rand() % height;
+}
+
+void RestartGame() {
+    if (gameOver) {
+        snake.clear();
+        snake.push_back({ width / 2, height / 2 });
+        CreateFood();
+        foodCount = 0;
+        gameOver = false;
+        SetWindowText(hWnd, L"SnakeGame");
+        InvalidateRect(hWnd, nullptr, TRUE);
+        SetFocus(hWnd); // Вернуть фокус на окно игры
+
+        // Включить таймер снова
+        SetTimer(hWnd, 1, 100, NULL);
+    }
+}
+
